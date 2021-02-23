1317a79e2d8f648f0d486a4ce728d037cadc09dc
bv4791 <bv4791@gmail.com>
2021-02-23 19:45:07 +0200

    Add Barbers migration

--- app.rb
+++ app.rb
@@ -7,8 +7,10 @@ require "sinatra/activerecord"
 set :database, {adapter: "sqlite3", database: "foo.sqlite3"}
 
 class Client < ActiveRecord::Base
-
 end	
+
+class Barber < ActiveRecord::Base
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_200927) do
+ActiveRecord::Schema.define(version: 2021_02_23_172934) do
+
+  create_table "barbers", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "client", force: :cascade do |t|
     t.text "name"

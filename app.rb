2cd5ffdbc42304725adde13df390befb62e5204c
bv4791 <bv4791@gmail.com>
2021-02-25 14:12:13 +0200

    Add length validation

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require "sinatra/activerecord"
 set :database, {adapter: "sqlite3", database: "foo.sqlite3"}
 
 class Client < ActiveRecord::Base
-		validates :name, presence: true
+		validates :name, presence: true, length: {minimum: 3}
 		validates :phone, presence: true
 		validates :datestamp, presence: true
 		validates :color, presence: true

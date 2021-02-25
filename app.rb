bc7d135d5af7e11ffd149d713984f625824a6707
bv4791 <bv4791@gmail.com>
2021-02-25 09:54:46 +0200

    Save client information the lame way

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-#encoding: utf-8
+
 require "rubygems"
 require "sinatra"
 require "sinatra/reloader"
@@ -12,7 +12,33 @@ end
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
+before do
 	@barbers = Barber.all
+end	
+
+get '/' do	
 	erb :index			
+end
+
+get '/visit' do
+    erb :visit
+end
+
+post "/visit" do
+	@username = params[ :username]
+	@phone = params[ :phone]
+	@datetime = params[ :datetime]
+	@barber = params[ :barber]
+	@color = params[ :color]
+
+	c = Client.new
+	c.name = @username
+	c.phone = @phone
+	c.datestamp = @datetime
+	c.barber = @barber
+	c.color = @color
+	c.save
+
+	erb  "<h2>Спасибо что записались</h2>"
+
 end
